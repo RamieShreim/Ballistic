fddc27f6d03f1333e0ccd8ffa155b9005750303f
Teo Lee <ttslee@users.noreply.github.com>
2019-02-17 14:10:15 -0800

    removed bad background

--- Scripts/BannerControl.gd
+++ Scripts/BannerControl.gd
@@ -8,14 +8,27 @@ var damage_2_prev: int = 0
 onready var player_1 = get_tree().get_root().get_node("Scene").get_node("Player1")
 onready var player_2 = get_tree().get_root().get_node("Scene").get_node("Player2")
 
+var drawn = false
+func drawTextures():
+	var sprite_1 = $PlayerOneControl/SpriteOne
+	var sprite_2 = $PlayerTwoControl/SpriteTwo
+	var player_1_sprite = player_1.get_node("Sprite")
+	var player_2_sprite = player_2.get_node("Sprite")
+	sprite_1.draw_texture(player_1_sprite, Vector2.ZERO)
+	sprite_2.draw_texture(player_2_sprite, Vector2.ZERO)
+	
 onready var label_1 = $PlayerOneControl/Percentage
 onready var label_2 = $PlayerTwoControl/Percentage
 
 func _ready():
+	
 	pass # Replace with function body.
 
 
 func _process(delta):
+	if !drawn:
+		drawTextures()
+		drawn = true
 	damage_1 = player_1.damage
 	if damage_1 != damage_1_prev:
 		damage_1_prev = damage_1
