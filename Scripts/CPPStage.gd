1c162b44be6014f7483220912e939190164119a2
Duncan Sparks <duncan.sprks@gmail.com>
2019-03-02 07:45:22 -0800

    Progress on CPP stage

--- Scripts/CPPStage.gd
+++ Scripts/CPPStage.gd
@@ -3,7 +3,7 @@ extends Node2D
 var code_typing: bool = false
 var chars: float = 0
 
-onready var code = $Code
+onready var code = $CanvasLayer/Code
 
 func _ready():
 	randomize()
@@ -14,35 +14,38 @@ func _ready():
 
 func _process(delta):
 	if code_typing:
+		code.modulate.a = 1
 		chars += 6 * delta
 		code.set_visible_characters(round(chars))
+	else:
+		code.modulate.a = max(code.modulate.a - 3 * delta, 0)
 
 
 func _on_TimerEvents_timeout():
 	match int(rand_range(0, 5)):
 		0: # Wall
-			$Code.set_text("#define WALL")
+			code.set_text("#define WALL")
 			$TimerWall.start()
 		1: # Bullets
-			$Code.set_text("#define BULLET_1\n#define BULLET_2\n#define BULLET_3")
+			code.set_text("#define BULLET_1\n#define BULLET_2\n#define BULLET_3")
 		2: # Block
-			$Code.set_text("#define BLOCK")
+			code.set_text("#define BLOCK")
 			$TimerBlock.start()
 		3: # Remove center
-			$Code.set_text("#undef CENTER")
+			code.set_text("#undef CENTER")
 			$TimerCenter.start()
 		4: # Comment
 			match int(rand_range(0, 5)):
 				0:
-					$Code.set_text("// HELLO THERE!!")
+					code.set_text("// HELLO THERE!!")
 				1:
-					$Code.set_text("// This is very exciting.")
+					code.set_text("// This is very exciting.")
 				2:
-					$Code.set_text("// Bounce! Bounce! Bounce!")
+					code.set_text("// Bounce! Bounce! Bounce!")
 				3:
-					$Code.set_text("std::printf(\"%s\", myDisappointment);")
+					code.set_text("std::printf(\"%s\", myDisappointment);")
 				4:
-					$Code.set_text("// You're starting to bore me.")
+					code.set_text("// You're starting to bore me.")
 			
 	#$Code.set_text("#define BLOCK")
 	
@@ -53,14 +56,21 @@ func _on_TimerWall_timeout():
 	$Wall.set_position(Vector2(rand_range(270, 1000), 334))
 	$Wall.show()
 	$Wall/CollisionShape2D.set_disabled(false)
+	$TimerFadeCode.start()
 
 
 func _on_TimerBlock_timeout():
 	$Block.set_position(Vector2(rand_range(224, 1066), rand_range(240, 400)))
 	$Block.show()
 	$Block/CollisionShape2D.set_disabled(false)
+	$TimerFadeCode.start()
 
 
 func _on_TimerCenter_timeout():
 	$Center.hide()
 	$Center/CollisionShape2D.set_disabled(true)
+	$TimerFadeCode.start()
+
+
+func _on_TimerFadeCode_timeout():
+	code_typing = false
